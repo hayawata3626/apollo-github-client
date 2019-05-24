f197937bd611de6569908e64956eb3234551032c
早津渉 <top.men09@gmail.com>
2019-05-24 09:25:57 +0900

    queryを記述

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const query = gql`
+  query($searchText: String!) {
+    search(first: 10, query: $searchText, type: REPOSITORY) {
+      nodes {
+        ... on Repository {
+          id
+          name
+          description
+          url
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`;
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Query query={query} variables={{ searchText: "apollo" }}>
+        {({ data }: any) => {
+          console.log(data);
+          return <div>ほげ</div>;
+        }}
+      </Query>
     </div>
   );
-}
+};
 
 export default App;
