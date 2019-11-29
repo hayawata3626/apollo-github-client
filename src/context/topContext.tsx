cfd03144cd444dc352abda1e03615d57a1132193
早津渉 <hayatsu_wataru@MPC-1168.local>
2019-11-29 09:56:57 +0900

    apolloからデータを取得しcontextでコンポーネントに渡すようにした

--- src/context/topContext.tsx
+++ src/context/topContext.tsx
@@ -1,9 +1,56 @@
-import React from "react";
+import gql from "graphql-tag";
+import React, { useContext } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { CircularProgress, Typography } from "@material-ui/core";
+import {
+  GetRepositoriesQuery,
+  GetRepositoriesQueryVariables
+} from "../grpahql/generated/type";
+// import GET_REPOSITORIES from "../graphql/query/getRepositories.graphql";
 
-export const TopContext = React.createContext<ReadonlyArray<string>>([]);
+const TopContext = React.createContext<any>([]);
+
+const GET_REPOSITORIES = gql`
+  query GetRepositories($searchText: String!) {
+    search(first: 10, query: $searchText, type: REPOSITORY) {
+      nodes {
+        ... on Repository {
+          id
+          name
+          description
+          url
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`;
+
+export function useTop() {
+  const context = useContext(TopContext);
+  if (!context) {
+    throw new Error(`useCount must be used within a CountProvider`);
+  }
+  return context;
+}
 
 export const TopProvider: React.FC = ({ children }) => {
   // TODO: ここでreadQueryやreadFragmentを使う。また{1: {id:1, ...}}のようなmapping処理も
-  const fruits = ["apple", "grape", "banana"];
-  return <TopContext.Provider value={fruits}>{children}</TopContext.Provider>;
+  const { data, error } = useQuery<
+    GetRepositoriesQuery,
+    GetRepositoriesQueryVariables
+  >(GET_REPOSITORIES, {
+    variables: { searchText: "react" }
+  });
+
+  if (!data) {
+    return <CircularProgress />;
+  }
+
+  if (error) {
+    return <Typography>{error.message}</Typography>;
+  }
+  return <TopContext.Provider value={data}>{children}</TopContext.Provider>;
 };
