e952191730435b94e5abf8e01faa1e8f79422068
早津渉 <top.men09@gmail.com>
2019-12-09 23:10:56 +0900

    Error Boundaryを導入

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { TopPage } from "./page/TopPage";
 import { TopProvider } from "./context/top/topContext";
+import { ErrorBoundary } from "./component/common/ErrorBoundary";
 // import getRepositories from "./graphql/query/getRepositories.graphql";
 
 export const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <TopProvider>
-            <TopPage />
-          </TopProvider>
-        </Route>
-      </Switch>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <TopProvider>
+              <TopPage />
+            </TopProvider>
+          </Route>
+        </Switch>
+      </Router>
+    </ErrorBoundary>
   );
 };
---
+++ src/component/common/ErrorBoundary.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+type State = Readonly<{
+  hasError: boolean;
+}>;
+
+export class ErrorBoundary extends React.Component<{}, State> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error.message);
+    // You can also log the error to an error reporting service
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
